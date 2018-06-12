93a578b9b7361606de01f03049246a068122e461
Eric G <ericdgreene@gmail.com>
2018-06-12 13:10:43 -0700

    first commit, adding npm support

---
+++ scripts/core/game.js
@@ -0,0 +1,15 @@
+(function() {
+  function Init() {
+    console.log("Initializing game...");
+  }
+  function Start(){
+
+  }
+  function Update(){
+
+  }
+  function Main(){
+    
+  }
+  onload = Init;
+})();
