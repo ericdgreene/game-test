89c7744577a8453bde9346ee14cd111ea093b817
Eric D. Greene <ericdgreene@gmail.com>
2018-06-12 17:03:25 -0700

    game test

--- scripts/core/game.js
+++ scripts/core/game.js
@@ -1,15 +1,30 @@
-(function() {
-  function Init() {
-    console.log("Initializing game...");
-  }
-  function Start(){
-
-  }
-  function Update(){
-
-  }
-  function Main(){
-    
-  }
-  onload = Init;
+"use strict";
+(function () {
+    // global game variables
+    var canvas = document.getElementById("canvas");
+    var stage;
+    var helloLabel;
+    function Init() {
+        console.log("Initializing game ...");
+        Start();
+    }
+    function Start() {
+        console.log("Starting application ...");
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; // sets to 60 fps
+        createjs.Ticker.on("tick", Update); // call update function every 'tick' (frame)
+        Main();
+    }
+    function Update() {
+        stage.update(); // redraws the stage
+    }
+    function Main() {
+        console.log("Game started!");
+        helloLabel = new createjs.Text("CreateJS Game", "32px Arial, Helvetica", "#333333");
+        helloLabel.x = canvas.width * 0.5;
+        helloLabel.y = canvas.height * 0.5;
+        stage.addChild(helloLabel);
+    }
+    onload = Init;
 })();
+//# sourceMappingURL=game.js.map
---
+++ scripts/core/game.ts
@@ -0,0 +1,35 @@
+(function() {
+  // global game variables
+  let canvas: any = document.getElementById("canvas");
+  let stage: createjs.Stage;
+  let helloLabel: createjs.Text;
+
+  function Init(): void {
+    console.log("Initializing game ...");
+    Start();
+  }
+  function Start(): void {
+    console.log("Starting application ...");
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.framerate = 60; // sets to 60 fps
+    createjs.Ticker.on("tick", Update); // call update function every 'tick' (frame)
+    Main();
+  }
+  function Update(): void {
+    stage.update(); // redraws the stage
+  }
+  function Main(): void {
+    console.log("Game started!");
+
+    helloLabel = new createjs.Text(
+      "CreateJS Game",
+      "32px Arial, Helvetica",
+      "#333333"
+    );
+    helloLabel.x = canvas.width * 0.5;
+    helloLabel.y = canvas.height * 0.5;
+
+    stage.addChild(helloLabel);
+  }
+  onload = Init;
+})();
